b57128d7544102665194e2bf43672fd5e1b7c04f
alfinadisurya24@gmail.com <alfinadisurya24@gmail.com>
2019-04-23 21:08:48 +0700

    home jadi

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -8,6 +8,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<!-- css link -->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/homee.css")?>">
+	<!-- icon -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<!-- js link -->
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
@@ -15,10 +17,51 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 </head>
 <body>
-	<div class="container">
-		<div class="row row-no-gutters">
-			<div class="col-lg">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-6">
   			<img class="logohome" src="<?php echo base_url("uploads/image/logoHome.png")?>">
+			</div>
+			<div class="col-lg-6">
+  			<img class="laptop" src="<?php echo base_url("uploads/image/home.png")?>">
+			</div>		
+		</div>
+		<div class="row row-no-gutters">
+			<div class="col-md-6">
+  			<p class="des">
+  				PeTIK Koperasi merupakan layanan koperasi <br>
+				yang disediakan di Pesantren Teknologi informasi dan Komunikasi<br>
+				yang dimana membantu Anda berbelanja dengan harga yang lebih murah<br>
+				dan terpercaya
+  			</p>
+			</div>
+		</div>
+		<div class="row row-no-gutters">
+			<div class="col-md-4">
+				<a href=""><img class="inputcon" src="<?php echo base_url("uploads/image/inputIcon.png")?>"></a>
+			</div>
+			<div class="col-md-2">
+				<a href=""><img class="historycon" src="<?php echo base_url("uploads/image/historyIcon.png")?>"></a>
+			</div>
+			<div class="col-md-6">
+				<div class="footers">
+					<div>
+					<i class="fa fa-map-marker"></i>
+					<p><span>Jl. Mandor Basar No.54 Rangkapajaya,<br>Pancoran Mas Depok,16435</p>
+					</div><br><br>
+					<div>
+					<i class="fa fa-phone"></i>
+					<p>(021) 77668891</p>
+					</div><br>
+					<div>
+					<i class="fa fa-envelope"></i>
+					<p>koperasipetik@gmail.com</p>
+					</div><br>
+					<div>
+					<i class="fa fa-facebook"></i>
+					<p>Pesantren Petik</p><br>
+					<div>
+				</div>
 			</div>
 		</div>
 	</div>
