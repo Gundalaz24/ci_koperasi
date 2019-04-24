99cdccb78fa2bfc1b364f7b3df89eb52ecf66a1d
alfinadisurya24@gmail.com <alfinadisurya24@gmail.com>
2019-04-24 12:02:55 +0700

    inputa n jadi

--- application/views/input_belanja.php
+++ application/views/input_belanja.php
@@ -31,110 +31,32 @@
   </nav>
 
   <!-- Page Content -->
-  <div class="container">
-
-    <!-- Jumbotron Header -->
-    <header class="jumbotron my-4 py-3">
-          <h1>Input Data Koperasi</h1><br>        
-      <div class="row text-center">
-        <div class="col-lg-3 col-md-6 mb-4">
-          <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
-        <div class="card-body">
-          <a href="#" class="btn btn-info">Transaksi</a>
+  <div class="container py-5">
+    <div class="row">
+      <div class="col-md-6 offset-md-3">
+        <form>
+         <div class="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
         </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        </div>
+        <div class="form-check">
+          <input type="checkbox" class="form-check-input" id="exampleCheck1">
+          <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
       </div>
-        </div>
-
-        <div class="col-lg-3 col-md-6 mb-4">
-          <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
-        <div class="card-body">
-          <a href="#" class="btn btn-info">Investasi</a>
-        </div>
-      </div>
-        </div>
-
-        <div class="col-lg-3 col-md-6 mb-4">
-          <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
-        <div class="card-body">
-          <a href="#" class="btn btn-info">Peminjaman</a>
-        </div>
-      </div>
-        </div>
-
-        <div class="col-lg-3 col-md-6 mb-4">
-          <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
-        <div class="card-body">
-          <a href="<?= base_url()?>belanja" class="btn btn-info">Belanja</a>
-        </div>
-      </div>
-        </div>
     </div>
-    </header>
-
-    <!-- Jumbotron Header -->
-    <header class="jumbotron my-4 py-3">
-          <h1>Daftar Riwayat Koperasi</h1><br>
-
-      <div class="row text-center">
-        <div class="col-lg-4 col-md-6 mb-4">
-          <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
-            <div class="card-body">
-              <a href="#" class="btn btn-info">Transaksi</a>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-lg-4 col-md-6 mb-4">
-          <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
-            <div class="card-body">
-              <a href="#" class="btn btn-info">Investasi</a>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-lg-4 col-md-6 mb-4">
-          <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
-            <div class="card-body">
-              <a href="#" class="btn btn-info">Peminjaman</a>
-            </div>
-          </div>
-        </div>
-
-    </div>
-
-    <div class="row text-center">
-        <div class="col-lg-4 col-md-6 mb-4">
-          <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
-            <div class="card-body">
-              <a href="#" class="btn btn-info">Belanja</a>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-lg-4 col-md-6 mb-4">
-          <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
-            <div class="card-body">
-              <a href="#" class="btn btn-info">Anggota</a>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-lg-4 col-md-6 mb-4">
-          <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
-            <div class="card-body">
-              <a href="#" class="btn btn-info">Produk</a>
-            </div>
-          </div>
-        </div>
-
-    </div>
-    </header>
-
   </div>
   <!-- /.container -->
 
   <!-- Footer -->
-  <footer class="py-5 bg-dark">
+  <footer class="py-5 bg-dark fixed-bottom">
     <div class="container">
       <p class="m-0 text-center text-white">Copyright &copy; Pesantren Teknologi Informasi dan Komunikasi 2019</p>
     </div>
